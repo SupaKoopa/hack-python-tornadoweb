a65a794f7c2ef48cd94f7040dad0d382e4b2ca3b
Mario De Freitas <mariodef@gmail.com>
2015-02-02 16:42:08 +0200

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 __author__ = 'mario'
 
 from tornado.ioloop import IOLoop
-from tornado.web import RequestHandler, Application, url
+from tornado.web import RequestHandler, Application, url, StaticFileHandler
 from searchHandler import searchHandler
 
 class RootHandler(RequestHandler):
@@ -19,7 +19,10 @@ if __name__ == "__main__":
     app = make_app()
     app.settings.setdefault("template_path", "templates")
 
+    handlers = [(r'/static/(.*)', StaticFileHandler, {'path': "./static"}),
+                (r"/search.*", searchHandler)]
 
-    app.add_handlers("localhost", [(r"/search.*", searchHandler)])
-    app.listen(8888)
+
+    app.add_handlers("localhost", handlers)
+    app.listen(8889)
     IOLoop.current().start()
